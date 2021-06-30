e9e6613230f3383cd236d5a9fdd26ae81bd64536
Kaden Thompson <kmthomp5@svsu.edu>
2021-06-30 14:00:21 -0400

    finished supervisor started overview for officials

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,12 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+function CopyToClipboard(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+}
