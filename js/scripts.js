a75907fdd694d50fefae21d70adc99a3d9d16ead
Kaden Thompson <36507132+KadenThomp36@users.noreply.github.com>
2021-07-02 00:22:19 -0400

    tons of work on officials search functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,60 +5,48 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-document.addEventListener("DOMContentLoaded", function(event) { 
-class Header extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-      this.innerHTML = `
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-      <div class="container">       
-          <a class="navbar-brand" href="index.html">
-            Lincoln Township
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                  <li class="nav-item"><a class="nav-link" aria-current="page" href="index.html">Home</a></li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle active" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Officials</a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="Supervisor.html">Supervisor</a></li>
-                        <li><a class="dropdown-item" href="Clerk.html">Clerk</a></li>
-                        <li><a class="dropdown-item" href="Treasurer.html">Treasurer</a></li>
-                        <li><a class="dropdown-item" href="#">Trustees</a></li>
-                        <li><hr class="dropdown-divider"/></li>
-                        <li><a class="dropdown-item" href="#"> Elected Officials</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="#">Township Officials</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item"><a class="nav-link" href="#">Monthly Meetings</a></li>
-                  <li class="nav-item"><a class="nav-link" href="#">Forms, Permits & Policies</a></li>
-              </ul>
-          </div>
-      </div>
-  </nav>
-      `;
-    }
-  }
-  
-  customElezments.define('header-component', Header);
-});
 
 var buttonUp = () => {
   const input = document.querySelector(".searchbox-input");
-  const cards = document.getElementsByClassName("card");
+  const cards = document.getElementsByClassName("targ");
+  const oopsie = document.getElementsByClassName("oops");
   let filter = input.value
+  var hit = true;
+  oopsie[0].classList.add("d-none")
+  
   for (let i = 0; i < cards.length; i++) {
       let title = cards[i].querySelector(".card-body");
       if (title.innerText.toLowerCase().indexOf(filter.toLowerCase()) > -1) {
           cards[i].classList.remove("d-none")
+
+          hit = false
       } else {
           cards[i].classList.add("d-none")
+
       }
+
+  }
+  if (hit == true){
+    oopsie[0].classList.remove("d-none")
   }
 }
+
+class Header extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <header>
+    <div class="h-auto mt-5 d-flex align-items-center container">
+      <div class="row">
+        <div class="col">
+          <h1 class="text-light">Homepage</h1>
+          <h3 class="text-white-50">Lincoln Township, Midland Michigan</h3>
+        </div>
+      </div>
+    </div>
+    
+    </header>
+    `;
+  }
+}
+customElements.define('main-header', Header);
