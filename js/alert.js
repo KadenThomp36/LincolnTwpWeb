fab217bb4c586ce70ff0bb82833a68f62452afd0
Kaden Thompson <kadenthomp36@gmail.com>
2021-07-21 16:11:38 -0400

    fixed alert size for wide viewports

--- js/alert.js
+++ js/alert.js
@@ -2,10 +2,13 @@
 class Alert extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
-      <div class="alert alert-warning alert-dismissible fade show m-2" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-        <strong>Trash & Recycling</strong> is behind, check the news card <a class="alert-link" href="index.html">here</a> to learn more
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      <div class="alert alert-warning alert-dismissible fade show m-2 mx-auto" role="alert" style="max-width: 1300px">
+        <div style="width: 90%" class="text-center">
+          <strong>Trash & Recycling</strong> is behind, check the news card <a class="alert-link" href="index.html">here</a> to learn more
+        </div
+        <div>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
       </div>
       `;
     }
