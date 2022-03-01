52087ce0ed2d1d61f1cfc0f96e40bd8526ecbafb
Kaden Thompson <kadenthomp36@gmail.com>
2022-03-01 14:01:11 -0500

    trash behind alert added

--- js/alert.js
+++ js/alert.js
@@ -1,16 +1,17 @@
 
+//to enable/disable add 'd-none' to the class list in the first div
 class Alert extends HTMLElement {
     connectedCallback() {
-      this.innerHTML =
-      <div class="alert d-none alert-warning alert-dismissible fade show m-2 mx-auto" role="alert" style="max-width: 1300px; margin:50px;">
+      this.innerHTML =`
+      <div class="alert alert-warning alert-dismissible fade show m-2 mx-auto" role="alert" style="max-width: 1300px; margin:50px;">
         <div style="width: 90%" class="text-center">
           <strong>Trash & Recycling behind one week due to weather</strong>
-        </div
+        </div>
         <div>
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
       </div>
-      ;
+      `;
     }
   }
   customElements.define('lincoln-alert', Alert);
