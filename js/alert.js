410e1b0c467afa9a469b42da054a1a7dc0f30306
Kaden Thompson <kadenthomp36@gmail.com>
2022-03-02 11:28:58 -0500

    removed trash alert

--- js/alert.js
+++ js/alert.js
@@ -3,7 +3,7 @@
 class Alert extends HTMLElement {
     connectedCallback() {
       this.innerHTML =`
-      <div class="alert alert-warning alert-dismissible fade show m-2 mx-auto" role="alert" style="max-width: 1300px; margin:50px;">
+      <div class="alert alert-warning d-none alert-dismissible fade show m-2 mx-auto" role="alert" style="max-width: 1300px; margin:50px;">
         <div style="width: 90%" class="text-center">
           <strong>Trash & Recycling behind one week due to weather</strong>
         </div>
