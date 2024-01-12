a2ab3cb64f221badedd2dd152d53ac33a467ad0c
KadenThomp36 <kmthomp5@svsu.edu>
2024-01-11 19:49:41 -0500

    creating json

--- js/meetingData.js
+++ js/meetingData.js
@@ -1,87 +1,603 @@
 const meetingDataBoard = {
-    2024: [
-      {
-        month: "January",
-        day: "9th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "February",
-        day: "13th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "March",
-        day: "9th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "April",
-        day: "9th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "May",
-        day: "14th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "June",
-        day: "11th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "July",
-        day: "9th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "August",
-        day: "13th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "September",
-        day: "10th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "October",
-        day: "8th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "November",
-        day: "12th",
-        disabledMin: false,
-        disabledAgen: true
-      },
-      {
-        month: "December",
-        day: "10th",
-        disabledMin: false,
-        disabledAgen: true,
-        special: true,
-      },
-    ],
-    2023: [
-      {
-        month: "January",
-        day: "10th",
-        disabledMin: false,
-        disabledAgen: false
-      }
-    ]
-  }
-  
-  module.exports = meetingDataBoard;
+  2024: [
+    {
+      month: "January",
+      day: "9th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "February",
+      day: "13th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "March",
+      day: "9th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "April",
+      day: "9th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "May",
+      day: "14th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "June",
+      day: "11th", fsdafdsa
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "July",
+      day: "9th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "August",
+      day: "13th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "September",
+      day: "10th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "October",
+      day: "8th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "November",
+      day: "12th",
+      disabledMin: false,
+      disabledAgen: true,
+    },
+    {
+      month: "December",
+      day: "10th",
+      disabledMin: false,
+      disabledAgen: true,
+      special: true,
+    },
+  ],
+  2023: [
+    {
+      month: "January",
+      day: "10th",
+    },
+  ],
+  2009: [
+    {
+      month: "January",
+      day: "9th",
+      agendaPath: "brdmtgs\\2009b\\janagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "13th",
+      agendaPath: "brdmtgs\\2009b\\febagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "13th",
+      agendaPath: "brdmtgs\\2009b\\maragnd.htm",
+      minutesPath: "brdmtgs\\2009b\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "10th",
+      agendaPath: "brdmtgs\\2009b\\apragnd.htm",
+      minutesPath: "brdmtgs\\2009b\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "8th",
+      agendaPath: "brdmtgs\\2009b\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "12th",
+      agendaPath: "brdmtgs\\2009b\\junagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "10th",
+      agendaPath: "brdmtgs\\2009b\\julagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "14th",
+      agendaPath: "brdmtgs\\2009b\\augagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "11th",
+      agendaPath: "brdmtgs\\2009b\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "9th",
+      agendaPath: "brdmtgs\\2009b\\octagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "13th",
+      agendaPath: "brdmtgs\\2009b\\novagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "11th",
+      agendaPath: "brdmtgs\\2009b\\decagnd.htm",
+      minutesPath: "brdmtgs\\2009b\\decmin.htm",
+    },
+  ],
+  2008: [
+    {
+      month: "January",
+      day: "7th",
+      agendaPath: "brdmtgs\\2008b\\janagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "12th",
+      agendaPath: "brdmtgs\\2008b\\febagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "11th",
+      agendaPath: "brdmtgs\\2008b\\maragnd.htm",
+      minutesPath: "brdmtgs\\2008b\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "8th",
+      agendaPath: "brdmtgs\\2008b\\apragnd.htm",
+      minutesPath: "brdmtgs\\2008b\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "13th",
+      agendaPath: "brdmtgs\\2008b\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "10th",
+      agendaPath: "brdmtgs\\2008b\\junagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "8th",
+      agendaPath: "brdmtgs\\2008b\\julagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "12th",
+      agendaPath: "brdmtgs\\2008b\\augagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "9th",
+      agendaPath: "brdmtgs\\2008b\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "14th",
+      agendaPath: "brdmtgs\\2008b\\octagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "11th",
+      agendaPath: "brdmtgs\\2008b\\novagnd.htm",
+      minutesPath: "brdmtgs\\2008b\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "9th",
+      agendaPath: "brdmtgs\\2008b\\decagnd.pdf",
+      minutesPath: "brdmtgs\\2008b\\decmin.htm",
+    },
+  ],
+  2007: [
+    {
+      month: "January",
+      day: "9th",
+      agendaPath: "brdmtgs\\2007b\\janagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "13th",
+      agendaPath: "brdmtgs\\2007b\\febagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "13th",
+      agendaPath: "brdmtgs\\2007b\\maragnd.htm",
+      minutesPath: "brdmtgs\\2007b\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "10th",
+      agendaPath: "brdmtgs\\2007b\\apragnd.htm",
+      minutesPath: "brdmtgs\\2007b\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "8th",
+      agendaPath: "brdmtgs\\2007b\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "12th",
+      agendaPath: "brdmtgs\\2007b\\junagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "10th",
+      agendaPath: "brdmtgs\\2007b\\julagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "14th",
+      agendaPath: "brdmtgs\\2007b\\augagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "11th",
+      agendaPath: "brdmtgs\\2007b\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "9th",
+      agendaPath: "brdmtgs\\2007b\\octagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "13th",
+      agendaPath: "brdmtgs\\2007b\\novagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "11th",
+      agendaPath: "brdmtgs\\2007b\\decagnd.htm",
+      minutesPath: "brdmtgs\\2007b\\decmin.htm",
+    },
+  ],
+  2006: [
+    {
+      month: "January",
+      day: "10th",
+      agendaPath: "brdmtgs\\2006b\\janagnd\\janagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\janmin\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "14th",
+      agendaPath: "brdmtgs\\2006b\\febagnd\\febagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\febmin\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "14th",
+      agendaPath: "brdmtgs\\2006b\\maragnd\\maragnd.htm",
+      minutesPath: "brdmtgs\\2006b\\marmin\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "11th",
+      agendaPath: "brdmtgs\\2006b\\apragnd\\apragnd.htm",
+      minutesPath: "brdmtgs\\2006b\\aprmin\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "9th",
+      agendaPath: "brdmtgs\\2006b\\mayagnd\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\maymin\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "13th",
+      agendaPath: "brdmtgs\\2006b\\junagnd\\junagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\junmin\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "11th",
+      agendaPath: "brdmtgs\\2006b\\julagnd\\julagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\julmin\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "8th",
+      agendaPath: "brdmtgs\\2006b\\augagnd\\augagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\augmin\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "12th",
+      agendaPath: "brdmtgs\\2006b\\sepagnd\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\sepmin\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "10th",
+      agendaPath: "brdmtgs\\2006b\\octagnd\\octagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\octmin\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "14th",
+      agendaPath: "brdmtgs\\2006b\\novagnd\\novagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\novmin\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "12th",
+      agendaPath: "brdmtgs\\2006b\\decagnd\\decagnd.htm",
+      minutesPath: "brdmtgs\\2006b\\decmin\\decmin.htm",
+    },
+  ],
+  2005: [
+    {
+      month: "January",
+      day: "11th",
+      agendaPath: "brdmtgs\\2005b\\janagnd\\janagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\janmin\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "8th",
+      agendaPath: "brdmtgs\\2005b\\febagnd\\febagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\febmin\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "8th",
+      agendaPath: "brdmtgs\\2005b\\maragnd\\maragnd.htm",
+      minutesPath: "brdmtgs\\2005b\\marmin\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "12th",
+      agendaPath: "brdmtgs\\2005b\\apragnd\\apragnd.htm",
+      minutesPath: "brdmtgs\\2005b\\aprmin\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "10th",
+      agendaPath: "brdmtgs\\2005b\\mayagnd\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\maymin\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "14th",
+      agendaPath: "brdmtgs\\2005b\\junagnd\\junagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\junmin\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "12th",
+      agendaPath: "brdmtgs\\2005b\\julagnd\\julagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\julmin\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "9th",
+      agendaPath: "brdmtgs\\2005b\\augagnd\\augagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\augmin\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "13th",
+      agendaPath: "brdmtgs\\2005b\\sepagnd\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\sepmin\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "11th",
+      agendaPath: "brdmtgs\\2005b\\octagnd\\octagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\octmin\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "8th",
+      agendaPath: "brdmtgs\\2005b\\novagnd\\novagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\novmin\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "13th",
+      agendaPath: "brdmtgs\\2005b\\decagnd\\decagnd.htm",
+      minutesPath: "brdmtgs\\2005b\\decmin\\decmin.htm",
+    },
+  ],
+  2004: [
+    {
+      month: "January",
+      day: "13th",
+      agendaPath: "brdmtgs\\2004b\\janagnd\\janagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\janmin\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "10th",
+      agendaPath: "brdmtgs\\2004b\\febagnd\\febagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\febmin\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "9th",
+      agendaPath: "brdmtgs\\2004b\\maragnd\\maragnd.htm",
+      minutesPath: "brdmtgs\\2004b\\marmin\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "13th",
+      agendaPath: "brdmtgs\\2004b\\apragnd\\apragnd.htm",
+      minutesPath: "brdmtgs\\2004b\\aprmin\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "11th",
+      agendaPath: "brdmtgs\\2004b\\mayagnd\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\maymin\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "8th",
+      agendaPath: "brdmtgs\\2004b\\junagnd\\junagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\junmin\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "13th",
+      agendaPath: "brdmtgs\\2004b\\julagnd\\julagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\julmin\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "10th",
+      agendaPath: "brdmtgs\\2004b\\augagnd\\augagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\augmin\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "14th",
+      agendaPath: "brdmtgs\\2004b\\sepagnd\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\sepmin\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "12th",
+      agendaPath: "brdmtgs\\2004b\\octagnd\\octagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\octmin\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "9th",
+      agendaPath: "brdmtgs\\2004b\\novagnd\\novagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\novmin\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "14th",
+      agendaPath: "brdmtgs\\2004b\\decagnd\\decagnd.htm",
+      minutesPath: "brdmtgs\\2004b\\decmin\\decmin.htm",
+    },
+  ],
+  2003: [
+    {
+      month: "January",
+      day: "15th",
+      agendaPath: "brdmtgs\\2003b\\janagnd\\janagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\janmin\\janmin.htm",
+    },
+    {
+      month: "February",
+      day: "11th",
+      agendaPath: "brdmtgs\\2003b\\febagnd\\febagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\febmin\\febmin.htm",
+    },
+    {
+      month: "March",
+      day: "11th",
+      agendaPath: "brdmtgs\\2003b\\maragnd\\maragnd.htm",
+      minutesPath: "brdmtgs\\2003b\\marmin\\marmin.htm",
+    },
+    {
+      month: "April",
+      day: "8th",
+      agendaPath: "brdmtgs\\2003b\\apragnd\\apragnd.htm",
+      minutesPath: "brdmtgs\\2003b\\aprmin\\aprmin.htm",
+    },
+    {
+      month: "May",
+      day: "13th",
+      agendaPath: "brdmtgs\\2003b\\mayagnd\\mayagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\maymin\\maymin.htm",
+    },
+    {
+      month: "June",
+      day: "10th",
+      agendaPath: "brdmtgs\\2003b\\junagnd\\junagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\junmin\\junmin.htm",
+    },
+    {
+      month: "July",
+      day: "8th",
+      agendaPath: "brdmtgs\\2003b\\julagnd\\julagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\julmin\\julmin.htm",
+    },
+    {
+      month: "August",
+      day: "12th",
+      agendaPath: "brdmtgs\\2003b\\augagnd\\augagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\augmin\\augmin.htm",
+    },
+    {
+      month: "September",
+      day: "9th",
+      agendaPath: "brdmtgs\\2003b\\sepagnd\\sepagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\sepmin\\sepmin.htm",
+    },
+    {
+      month: "October",
+      day: "14th",
+      agendaPath: "brdmtgs\\2003b\\octagnd\\octagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\octmin\\octmin.htm",
+    },
+    {
+      month: "November",
+      day: "11th",
+      agendaPath: "brdmtgs\\2003b\\novagnd\\novagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\novmin\\novmin.htm",
+    },
+    {
+      month: "December",
+      day: "9th",
+      agendaPath: "brdmtgs\\2003b\\decagnd\\decagnd.htm",
+      minutesPath: "brdmtgs\\2003b\\decmin\\decmin.htm",
+    },
+  ],
+};
+
+module.exports = meetingDataBoard;
