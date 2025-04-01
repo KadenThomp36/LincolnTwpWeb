95082a60c3e5f50aa193d4f3102ca9fd49d5eb25
Kaden Thompson <kadenthomp36@gmail.com>
2025-04-01 13:55:06 -0400

    added notice

--- js/alert.js
+++ js/alert.js
@@ -1,17 +1,17 @@
-
 //to enable/disable add 'd-none' to the class list in the first div
 class Alert extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML =`
-      <div class="alert alert-warning d-none alert-dismissible fade show m-2 mx-auto" role="alert" style="max-width: 1300px; margin:50px;">
-        <div style="width: 90%" class="text-center">
-          <strong>Trash & Recycling behind one week due to weather</strong>
-        </div>
-        <div>
-          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+  connectedCallback() {
+    this.innerHTML = `
+      <div class="alert alert-primary alert-dismissible fade show m-2 mx-auto d-flex align-items-center" role="alert" style="max-width: 1300px; margin:50px;">
+      <div style="width: 100%" class="text-center">
+      Notice of Public Hearing - May 6, 2025 
+      <button class="btn btn-link ps-4 p-0 text-decoration-underline" style="color: black;" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="setSrcTitle('assets/Misc/Notice.pdf', 'Notice Of Public Hearing')">Learn more</button>
+      </div>
+      <div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>
       </div>
       `;
-    }
   }
-  customElements.define('lincoln-alert', Alert);
+}
+customElements.define("lincoln-alert", Alert);
